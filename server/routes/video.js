61aa0fb4dcbf80349bc643dca9d1518ead262a6c
dh <hyunlove12@naver.com>
2020-04-07 11:50:54 +0900

    20200407

--- server/routes/video.js
+++ server/routes/video.js
@@ -53,7 +53,7 @@ router.post('/uploadVideo', (req, res) => {
     video.save((err, doc) => {
         if(err) return res.json({
             success : false, 
-            err
+            err 
         })
         res.status(200).json({ success : true })
     })
